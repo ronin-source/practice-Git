e9efa6e57600f5d2ed00b9e309ba2dd686442e54
ronin-source <roninbuilder@gmail.com>
2024-09-05 17:08:59 +0530

    subtract

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 def addition(a,b):
     return a+b
-def subtract(a,b):
+def subtract(a,b,c):
     if a>b:
-        return a-b
+        return a-b-c
     else:
         return b-a
 def division(a,b):
